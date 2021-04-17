6cc371b90ab2a8a57877803edeb225adee78f4ae
Coory22 <60852858+Coory22@users.noreply.github.com>
2021-04-17 11:02:15 -0500

    Update matrix_class.s

--- matrix_class.s
+++ matrix_class.s
@@ -1,5 +1,28 @@
 
-  
+
+.data
+.text
+Main:
+li a0, 4
+li a1, 3
+jal create_Matrix
+mv s0, a0
+li a1, 3
+li a2, 2
+li a3, 10
+jal set_Element
+mv a0, s0
+li a1, 3
+li a2, 2
+jal get_Element
+jal print_Int
+li a0, 4
+li a1, 3
+jal create_Matrix
+jal matrix_Addition
+j End
+
+
 ############################## Matrix #################################
 # nxm  4*n*m location on heap
 # Input : size m into a0, size of n in a1.
@@ -9,26 +32,6 @@
 # There will be an Add_Element to matrix method that will add to the location with the matrix being stored in a0, the row you want in a1, the columns want in a2, and the element want to be added in a3.  
 # There will be a Get_Element method that will get the element at the row and column specified. So, the matrix will be stored in a0,  the row you want will be stored in a1, and the column you want will be stored in a2. To get the element will do 4*(a1)*(number element in the row)+(a2)=(memory location) then return the value at the location on the data portion of the matrix in a0.
 
-
-
-Main:
-li a0, 4
-li a1, 3
-jal create_Matrix
-mv s0, a0
-li a1, 0
-li a2, 2
-li a3, 10
-jal set_Element
-mv a0, s0
-li a1, 0
-li a2, 2
-jal get_Element
-jal print_Int
-j End
-
-
-
 #Note:  rows start at zero
 create_Matrix:
 addi sp, sp, -12
@@ -37,9 +40,11 @@ sw a1, 4(sp)#number of colums a1
 sw ra, 8(sp)# stores return address
 
 # this allocates room on the heap
+addi a0, a0, 1
 mul a0, a0, a1
 li t0, 4 # to allocate 4 bytes to each element
 mul a0, a0, t0
+addi a0, a0, 4
 jal Malloc
 sw a0, 0(a0)
 
@@ -51,10 +56,29 @@ sw a1, 4(a0)
 lw a1, 4(sp)
 sw a1, 8(a0)
 
-
+# #intializes every element to zero
+# li t0, 0
+# li t1, 0
+# lw t2, 4(a0)
+# lw t3, 8(a0)
+# Loop_row_Create:        
+# 	Loop_columns_Cretate:
+# 		mv a1, t0
+#         mv a2, t1
+#         li a4, 0
+#         jal set_Element
+#         addi t1, t1, 1
+#         beq t1, t3, End_Loop_columns_Cretate
+#         j Loop_columns_Cretate
+#     End_Loop_columns_Cretate:
+#     li t1, 0    
+#     addi t0, t0, 1
+#     beq t0, t2, End_Loop_row_Create
+#    j Loop_row_Create
+# End_Loop_row_Create:
 lw ra, 8(sp) # set return address
 addi sp,sp,12
-ret 
+ret
 
 set_Element:
 #matrix being stored in a0
@@ -77,6 +101,7 @@ add a2, a2, a3
 li a3, 4
 mul a2, a2, a3
 add a1, a1, a2
+addi a1,a1, 4
 lw a2, 8(sp)
 sw a2, 0(a1)
 addi sp,sp,12
@@ -100,9 +125,65 @@ add a2, a2, a3
 li a3, 4
 mul a2, a2, a3
 add a1, a1, a2
+addi a1,a1,4
 lw a0, 0(a1)
 addi sp, sp, 8
 ret
+
+matrix_Addition:
+#matrix one is stored in a0
+#matrix two is stored in a1
+# returns resulting matrix in a0
+# the matricies should be the same size
+addi sp, sp, -16
+sw ra, 12(sp)
+sw a0, 0(sp)
+sw a1, 4(sp)
+# t5 is the row
+# t1 is the colium
+# t2 is the size of row
+# t3 is tha size of columns
+# t4 is the element from first matrix
+li t5, 0
+li t1, 0
+lw t2, 4(a0)
+lw t3, 8(a0)
+mv a0, t2
+mv a1, t3
+jal create_Matrix
+sw a0, 8(sp)
+Loop_row_add:        
+	Loop_columns_Add:
+    	lw a0, 0(sp)
+      	mv a1, t0
+        mv a2, t1
+        jal get_Element
+        mv t4, a0
+        lw a0, 4(sp)
+        mv a1, t0
+        mv a2, t1
+        jal get_Element
+        add a4, a0, t4
+        lw a0, 8(sp)
+        mv a1, t0
+        mv a2, t1
+        jal set_Element
+        addi t1, t1, 1
+        beq t1, t3, End_Loop_columns_Add
+        j Loop_columns_Add
+    End_Loop_columns_Add:
+    li t1, 0    
+    addi t5, t5, 1
+    beq t5, t2, End_Loop_row_add
+
+    j Loop_row_add
+End_Loop_row_add:
+lw a0, 8(sp)
+lw ra, 12(sp)
+addi sp, sp, 16
+ret
+
+
 
 
 Malloc:
